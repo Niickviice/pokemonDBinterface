a50582a0a95564ba4cb2553979241c5e7b4ddc2e
Alan Martinez <nickviice@gmail.com>
2024-03-05 22:47:14 -0600

    Se cambio el navbar y agregó un search form junto con un carrusel de imagenes

--- pokemon/pokemon/settings.py
+++ pokemon/pokemon/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "miapp"
+    "miapp",
+    "bootstrap5",
+    "django_bootstrap_icons",
+
+
 ]
 
 MIDDLEWARE = [
